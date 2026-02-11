b264084ad124d6c8b7b031405edac82f5b64240f
Areeba Yaseen <areebayaseen15@gmail.com>
2026-02-11 13:51:11 +0500

    text updated

--- src/components/about/DesignStudioAbout.tsx
+++ src/components/about/DesignStudioAbout.tsx
@@ -5,21 +5,20 @@ import Link from 'next/link';
 
 const DesignStudioAbout = () => {
     return (
-        <div id='about' className="des-about-area pb-80">
+        <div id ="about">
+        <div  className="des-about-area pb-80">
             <div className="container container-1230">
                 <div className="row justify-content-center">
                     <div className="col-xl-12">
                         <div className="des-about-text text-center">
                             <p className="tp_text_invert_3 mb-45">
-                                With over 10 years of experience as a versatile Front-End and Full-Stack
-                                Developer, I excel in crafting a wide range of solutions, from dynamic web
-                                applications and interactive configurators to robust backend automation
-                                software, ensuring innovative and effective outcomes for any project.
+                                Leveraging ten years of collective engineering expertise, we transform corporate branding into premium tools. Professional signatures are your digital handshake; our team applies deep technical backgrounds to build sharp, interactive HTML solutions that ensure your brand identity remains flawless for every client.
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
