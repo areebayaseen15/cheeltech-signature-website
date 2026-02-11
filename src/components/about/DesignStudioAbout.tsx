5dbe2e9eed5b8db0da066a31fb83658f52d8295d
Areeba Yaseen <areebayaseen15@gmail.com>
2026-02-11 13:30:03 +0500

    remove button

--- src/components/about/DesignStudioAbout.tsx
+++ src/components/about/DesignStudioAbout.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const DesignStudioAbout = () => {
     return (
-        <div id='about' className="des-about-area pb-200">
+        <div id='about' className="des-about-area pb-80">
             <div className="container container-1230">
                 <div className="row justify-content-center">
                     <div className="col-xl-12">
@@ -16,14 +16,6 @@ const DesignStudioAbout = () => {
                                 applications and interactive configurators to robust backend automation
                                 software, ensuring innovative and effective outcomes for any project.
                             </p>
-                            <Link className="tp-btn-border" href="/about-modern-light">
-                                More About Us
-                                <span>
-                                    <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1 9.99999H15.2222M8.11121 1.11108L17.0001 9.99997L8.11121 18.8889" stroke="currentcolor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                    </svg>
-                                </span>
-                            </Link>
                         </div>
                     </div>
                 </div>
