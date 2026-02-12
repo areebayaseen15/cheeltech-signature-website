8009896a0e60d855a3183758a8793c528f83a917
Areeba Yaseen <areebayaseen15@gmail.com>
2026-02-12 14:04:02 +0500

    added footer logo

--- src/layouts/footers/MobileApplicationFooter.tsx
+++ src/layouts/footers/MobileApplicationFooter.tsx
@@ -1,5 +1,5 @@
 import MobileApplicationCopyright from './subComponents/MobileApplicationCopyright';
-import logoBlack from '../../../public/assets/img/logo/logo-black.png';
+import logoBlack from '../../../public/assets/img/web-logo/sssss8.png';
 import { SendEmailIcon } from '@/svg';
 import Image from 'next/image';
 import Link from 'next/link';
